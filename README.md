e10838dca1a1136e8b4aa3bd2811d784b301ced7
Rodrigo Alcorta <rodrigovictoralcorta@gmail.com>
2022-06-07 18:57:17 -0300

    Init

---
+++ README.md
@@ -0,0 +1,16 @@
+# PERSONAL CARD
+
+This my NPX card unique style to connect with me directly via console or terminal
+
+👇 just hit
+
+```bash
+npx ralcorta
+```
+
+<hr/>
+
+##### STEPS TO CREATE YOUR OWN
+
+The article written by our friend @jackboberg. I used the same for the reference to deploy the package.
+[Write a Simple npx Business Card](https://studioelsa.se/blog/open-source-oss-npx-business-card).
---
+++ card.js
@@ -0,0 +1,105 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const boxen = require("boxen");
+const chalk = require("chalk");
+const inquirer = require("inquirer");
+const clear = require("clear");
+const open = require("open");
+const fs = require('fs');
+const request = require('request');
+const path = require('path');
+const ora = require('ora');
+const cliSpinners = require('cli-spinners');
+clear();
+
+const prompt = inquirer.createPromptModule();
+
+const questions = [
+    {
+        type: "list",
+        name: "action",
+        message: "What you want to do?",
+        choices: [
+            {
+                name: `Send me an ${chalk.green.bold("email")}?`,
+                value: () => {
+                    open("mailto:rodrigovictoralcorta@gmail.com");
+                    console.log("\nDone, see you soon at inbox.\n");
+                }
+            },
+            {
+                name: `Schedule a ${chalk.redBright.bold("Meeting")}?`,
+                value: () => {
+                    open('https://calendly.com/rodrigovictoralcorta/15min');
+                    console.log("\n See you at the meeting \n");
+                }
+            },
+            {
+                name: "Just quit.",
+                value: () => {
+                    console.log("Hasta la vista.\n");
+                }
+            }
+        ]
+    }
+];
+
+const data = {
+    name: chalk.bold.green("             Rodrigo Victor Alcorta"),
+    handle: chalk.white("@ralcorta"),
+    work: `${chalk.white("Software Engineer at")} ${chalk
+        .hex("#2b82b2")
+        .bold("Valiu Lab")}`,
+    github: chalk.gray("https://github.com/") + chalk.green("ralcorta"),
+    linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue("rodrigoalcorta"),
+    web: chalk.cyan("https://rodrigoalcorta.com"),
+    npx: chalk.red("npx") + " " + chalk.white("ralcorta"),
+
+    labelWork: chalk.white.bold("       Work:"),
+    labelGitHub: chalk.white.bold("     GitHub:"),
+    labelLinkedIn: chalk.white.bold("   LinkedIn:"),
+    labelWeb: chalk.white.bold("        Web:"),
+    labelCard: chalk.white.bold("       Card:")
+};
+
+const me = boxen(
+    [
+        `${data.name}`,
+        ``,
+        `${data.labelWork}  ${data.work}`,
+        ``,
+        `${data.labelGitHub}  ${data.github}`,
+        `${data.labelLinkedIn}  ${data.linkedin}`,
+        `${data.labelWeb}  ${data.web}`,
+        ``,
+        `${data.labelCard}  ${data.npx}`,
+        ``,
+        `${chalk.italic("my inbox is always open. Whether you have a")}`,
+        `${chalk.italic(
+            "question or just want to say hi, I will try "
+        )}`,
+        `${chalk.italic(
+            "my best to get back to you!"
+        )}`
+    ].join("\n"),
+    {
+        margin: 1,
+        float: 'center',
+        padding: 1,
+        borderStyle: "single",
+        borderColor: "green"
+    }
+);
+
+console.log(me);
+const tip = [
+    `Tip: Try ${chalk.cyanBright.bold(
+        "cmd/ctrl + click"
+    )} on the links above`,
+    '',
+].join("\n");
+console.log(tip);
+
+prompt(questions).then(answer => answer.action());
